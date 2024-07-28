9f0647cef6552e0170fcc0205b2ffca1a7d7ae4c
Marco Engelhardt <sirmarco@reussnet.ch>
2024-07-28 12:55:18 +0200

    board view

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-board',
   standalone: true,
   imports: [],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
 })
 export class BoardComponent {
+  tasks: any = [];
+  error: string = '';
+  constructor(private http: HttpClient) {}
 
+  async ngOnInit() {
+    try {
+      this.tasks = await this.loadTasks();
+      console.log(this.tasks);
+    } catch (error) {
+      this.error = 'fehler beim laden';
+    }
+  }
+
+  loadTasks() {
+    const url = environment.baseUrl + '/tasks/';
+    return lastValueFrom(this.http.get(url));
+  }
 }
